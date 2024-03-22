9f0d5a9c6d7068669c84d46b884000bbd1979689
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-22 17:36:37 +0100

    fix: missing alt in imgs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,20 @@ function Navbar() {
 	return (
 		<div className="w-[92vw] mx-auto mt-[5vh] py-[0.875rem] pl-6 pr-[0.875rem] flex justify-between items-center bg-grey-900 rounded-full border border-grey-800">
 			<a className="flex gap-1">
-				<img src={logo} />
-				<img src={logoText} />
+				<img
+					src={logo}
+					alt="YourBank logo"
+				/>
+				<img
+					src={logoText}
+					alt="YourBank name"
+				/>
 			</a>
 			<button className="px-3 py-1 rounded-full bg-green-800">
-				<img src={hamburgerIcon} />
+				<img
+					src={hamburgerIcon}
+					alt="menu icon"
+				/>
 			</button>
 		</div>
 	);
