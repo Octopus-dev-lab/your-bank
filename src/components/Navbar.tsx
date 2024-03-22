36d8101123e5744cdaebb7436679cab7e1d6078f
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-22 18:46:30 +0100

    fix: root translated downward
    Added an external div to Navbar.
    The external div is utilized to provide the navbar with enough distance from the top of the screen. Using margin (which would be the first margin in the file) would cause the root to be translated downward.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,23 +4,26 @@ import logoText from '../assets/your-bank.svg';
 
 function Navbar() {
 	return (
-		<div className="w-[92vw] mx-auto mt-[5vh] py-[0.875rem] pl-6 pr-[0.875rem] flex justify-between items-center bg-grey-900 rounded-full border border-grey-800">
-			<a className="flex gap-1">
-				<img
-					src={logo}
-					alt="YourBank logo"
-				/>
-				<img
-					src={logoText}
-					alt="YourBank name"
-				/>
-			</a>
-			<button className="px-3 py-1 rounded-full bg-green-800">
-				<img
-					src={hamburgerIcon}
-					alt="menu icon"
-				/>
-			</button>
+		// The external div is utilized to provide the navbar with enough distance from the top of the screen. Using margin (which would be the first margin in the file) would cause the root to be translated downward.
+		<div className="w-full pt-[5vh]">
+			<div className="w-[92%] mx-auto py-[0.875rem] pl-6 pr-[0.875rem] flex justify-between items-center bg-grey-900 rounded-full border border-grey-800">
+				<a className="flex gap-1">
+					<img
+						src={logo}
+						alt="YourBank logo"
+					/>
+					<img
+						src={logoText}
+						alt="YourBank name"
+					/>
+				</a>
+				<button className="px-3 py-1 rounded-full bg-green-800">
+					<img
+						src={hamburgerIcon}
+						alt="menu icon"
+					/>
+				</button>
+			</div>
 		</div>
 	);
 }
