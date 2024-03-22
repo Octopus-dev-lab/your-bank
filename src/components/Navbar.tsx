2d249b517334df51a0e561cd1a3361cb4554afef
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-22 17:19:12 +0100

    feat: Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import hamburgerIcon from '../assets/hamburger.svg';
+import logo from '../assets/logo.svg';
+import logoText from '../assets/your-bank.svg';
+
+function Navbar() {
+	return (
+		<div className="w-[92vw] mx-auto mt-[5vh] py-[0.875rem] pl-6 pr-[0.875rem] flex justify-between items-center bg-grey-900 rounded-full border border-grey-800">
+			<a className="flex gap-1">
+				<img src={logo} />
+				<img src={logoText} />
+			</a>
+			<button className="px-3 py-1 rounded-full bg-green-800">
+				<img src={hamburgerIcon} />
+			</button>
+		</div>
+	);
+}
+
+export default Navbar;
