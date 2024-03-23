85d1802e92f9d0d549705612150c984edaf466e9
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-23 16:44:53 +0100

    feat: Preview component

---
+++ src/components/Preview.tsx
@@ -0,0 +1,21 @@
+import arrowDesign from '../assets/preview-arrow-abstract.svg';
+import PreviewBox from './PreviewBox';
+import PreviewCurrencyChip from './PreviewCurrencyChip';
+import PreviewFloatingChip from './PreviewFloatingChip';
+
+function Preview() {
+	return (
+		<div className="relative w-[92vw] mx-auto mt-20">
+			<PreviewFloatingChip />
+			<img
+				className="absolute -top-[1.19rem] -right-[1.31rem] -z-40"
+				src={arrowDesign}
+				alt="arrow design element"
+			/>
+			<PreviewBox />
+			<PreviewCurrencyChip />
+		</div>
+	);
+}
+
+export default Preview;
