eb6e02f99b0c13cf5cabb67970ecffd4d51ac966
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-23 18:34:58 +0100

    fix: moved Introduction assets to dedicated folder

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,4 +1,5 @@
-import checkIcon from '../assets/check.svg';
+import checkIcon from '../assets/home.introduction/check.svg';
+
 function Chip() {
 	return (
 		<div className="w-fit pl-2 pr-4 py-2 mb-3 gap-1 flex justify-center items-center bg-grey-800 rounded-full">
