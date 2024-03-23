d79897c3879dcc3f1687befc9c3d2d5ced6f3dfa
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-23 11:41:09 +0100

    feat: Chip component

---
+++ src/components/Chip.tsx
@@ -0,0 +1,16 @@
+import checkIcon from '../assets/check.svg';
+function Chip() {
+	return (
+		<div className="w-fit pl-2 pr-4 py-2 mb-3 gap-1 flex justify-center items-center bg-grey-800 rounded-full">
+			<img
+				src={checkIcon}
+				alt="check icon"
+			/>
+			<p className="text-white-100 text-xs leading-6 font-extralight">
+				No LLC Required, No Credit Check.
+			</p>
+		</div>
+	);
+}
+
+export default Chip;
