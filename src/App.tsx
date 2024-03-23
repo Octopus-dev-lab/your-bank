142c42bf4e2ecc265c15c40e757a4aad67a88424
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-23 11:42:24 +0100

    feat: added abstract design and Home to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import abstract from './assets/home-topleft-abstract.svg';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
 	return (
 		<>
+			<img
+				className="absolute top-0 left-0 -z-10"
+				src={abstract}
+				alt="abstract design"
+			/>
 			<Navbar />
+			<Home />
 		</>
 	);
 }
