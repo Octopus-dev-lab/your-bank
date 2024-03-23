3b9b8f97cdda3e27588ba8cd01551ff227357b5b
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-23 11:41:49 +0100

    feat: Home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Introduction from '../components/Introduction';
+import Preview from '../components/Preview';
+
+function Home() {
+	return (
+		<div className="mt-12">
+			<Introduction />
+			<Preview />
+		</div>
+	);
+}
+
+export default Home;
