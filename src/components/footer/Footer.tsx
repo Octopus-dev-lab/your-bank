4add22d4edc738f7760f6108ccd092ab6343c1d7
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-24 19:03:07 +0100

    feat: Footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import Contacts from './Contacts';
+import InfoExtra from './InfoExtra';
+import Navigation from './Navigation';
+
+function Footer() {
+	return (
+		<footer className="mt-20 pt-[3.12rem] pb-[1.88rem] px-4 -z-10 flex flex-col justify-center items-center bg-grey-900">
+			<Navigation />
+			<div className="w-full h-px my-[30px] bg-grey-800"></div>
+			<Contacts />
+			<div className="w-full h-px mt-[30px] mb-[50px] bg-grey-800"></div>
+			<InfoExtra />
+		</footer>
+	);
+}
+
+export default Footer;
