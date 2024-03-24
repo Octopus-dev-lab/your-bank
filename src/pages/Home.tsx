61605cd80bd8cefae3704f96be8798db57a66410
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-24 11:40:26 +0100

    feat: added Products to Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Introduction from '../components/Introduction';
 import Preview from '../components/Preview';
+import Products from '../components/Products';
 
 function Home() {
 	return (
 		<div className="mt-12">
 			<Introduction />
 			<Preview />
+			<Products />
 		</div>
 	);
 }
