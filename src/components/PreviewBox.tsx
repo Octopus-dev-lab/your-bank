d2c3cb3d6138c7cde49249bcb2dbfdd73a3383cc
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-24 17:49:14 +0100

    fix: bg texture

--- src/components/PreviewBox.tsx
+++ src/components/PreviewBox.tsx
@@ -7,7 +7,7 @@ function PreviewBox() {
 		<div className="relative w-[85%] mx-auto p-px -z-30 rounded-md bg-gradient-to-br from-grey-800 via-[#3A441F] to-grey-800 from-[37%] to-[63%]">
 			<div className="relative rounded-md bg-[#1A1A1A] -z-20">
 				<img
-					className="absolute w-full h-full -z-10  opacity-30"
+					className="absolute w-full h-full -z-10  opacity-80"
 					src={bgTexture}
 				></img>
 				<div className="p-[1.28rem] flex flex-col gap-4 rounded-md bg-gradient-to-tr from-[#1A1A1A] from-55%">
