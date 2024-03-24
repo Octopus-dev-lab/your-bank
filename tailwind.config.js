be3211b7a4e7fd60219fc296bca392f60f6affab
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-24 17:50:43 +0100

    feat: tailwind plugin gradient angle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin';
+
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
@@ -44,5 +46,33 @@ export default {
 			sans: ['Lexend', 'sans-serif']
 		}
 	},
-	plugins: []
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'bg-gradient': (angle) => ({
+						'background-image': `linear-gradient(${angle} , var(--tw-gradient-stops))`
+					})
+				},
+				{
+					values: Object.assign(
+						theme('bgGradientDeg', {}), // name of config key. Must be unique
+						{
+							10: '10deg', // bg-gradient-10
+							15: '15deg',
+							20: '20deg',
+							25: '25deg',
+							30: '30deg',
+							45: '45deg',
+							60: '60deg',
+							90: '90deg',
+							120: '120deg',
+							135: '135deg',
+							240: '240deg'
+						}
+					)
+				}
+			);
+		})
+	]
 };
