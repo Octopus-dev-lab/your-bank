ea7e4f965f568ec068114f9b862d2598ca76431f
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-24 15:45:25 +0100

    feat: added customCss prop

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,14 +1,18 @@
 function Toggle<T>({
+	customCss = '',
 	labels,
 	active,
 	setActive
 }: {
+	customCss?: string;
 	labels: Record<string, T>;
 	active: T;
 	setActive: (active: T) => void;
 }) {
 	return (
-		<div className="p-3 rounded-full bg-grey-900 border border-grey-800 mb-[3.75rem]">
+		<div
+			className={`p-3 rounded-full bg-grey-900 border border-grey-800 mb-[3.75rem] ${customCss}`}
+		>
 			{Object.keys(labels).map((key) => (
 				<button
 					key={key}
