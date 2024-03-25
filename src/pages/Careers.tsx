486d100f42b5555fc7a454176b2967be86487714
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-25 11:39:35 +0100

    feat: added Values component to Careers

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,9 +1,11 @@
 import Introduction from '../components/careers.introduction/Introduction';
+import Values from '../components/careers.values/Values';
 
 function Careers() {
 	return (
 		<div className="mt-[1.87rem]">
 			<Introduction />
+			<Values />
 		</div>
 	);
 }
