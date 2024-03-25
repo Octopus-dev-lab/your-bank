4d6c8411839a724b067b2f35241253117dd7efa3
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-25 10:45:58 +0100

    feat: moved top-left abstract design into Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import abstract from './assets/home-topleft-abstract.svg';
+// import abstract from './assets/home-topleft-abstract.svg';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home';
@@ -6,11 +6,6 @@ import Home from './pages/Home';
 function App() {
 	return (
 		<>
-			<img
-				className="absolute top-0 left-0 -z-10"
-				src={abstract}
-				alt="abstract design"
-			/>
 			<Navbar />
 			<Home />
 			<Footer />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import abstract from '../assets/home/home-topleft-abstract.svg';
 import Faqs from '../components/faqs/Faqs';
 import Features from '../components/features/Features';
 import Introduction from '../components/introduction/Introduction';
@@ -9,6 +10,11 @@ import Testimonials from '../components/testimonials/Testimonials';
 function Home() {
 	return (
 		<div className="mt-12 mb-20">
+			<img
+				className="absolute top-0 left-0 -z-10"
+				src={abstract}
+				alt="abstract design"
+			/>
 			<Introduction />
 			<Preview />
 			<Products />
