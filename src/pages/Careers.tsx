3e281f35344ee76904b30e31799922389bd37f5d
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-25 11:16:23 +0100

    feat: Careers page component

---
+++ src/pages/Careers.tsx
@@ -0,0 +1,11 @@
+import Introduction from '../components/careers.introduction/Introduction';
+
+function Careers() {
+	return (
+		<div className="mt-[1.87rem]">
+			<Introduction />
+		</div>
+	);
+}
+
+export default Careers;
