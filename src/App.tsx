a4127a92c4d9d51c23d19a551e1ad7f3c4c8ee1c
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-25 11:16:40 +0100

    feat: added Careers to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-// import abstract from './assets/home-topleft-abstract.svg';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
-import Home from './pages/Home';
+import Careers from './pages/Careers';
+// import Home from './pages/Home';
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			<Home />
+			{/* <Home /> */}
+			<Careers />
 			<Footer />
 		</>
 	);
