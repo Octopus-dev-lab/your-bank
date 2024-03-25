238579bb77f3ecc6cde67ddbf78bc62dc7cd6e5c
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-25 16:08:56 +0100

    feat: added benefits data

---
+++ src/components/careers.benefits/benefits.data.ts
@@ -0,0 +1,35 @@
+import briefcaseIcon from '../../assets/careers.benefits/briefcase.svg';
+import documentsIcon from '../../assets/careers.benefits/documents.svg';
+import lampIcon from '../../assets/careers.benefits/lamp.svg';
+import presetationIcon from '../../assets/careers.benefits/presentation.svg';
+
+export default [
+	{
+		icon: presetationIcon,
+		alt: 'presentation icon',
+		title: 'Competitive Compensation',
+		description:
+			'We provide a competitive salary package that recognizes the skills and expertise of our employees. YourBank believes in rewarding exceptional performance and offering opportunities for financial growth.'
+	},
+	{
+		icon: lampIcon,
+		alt: 'lamp icon',
+		title: 'Health and Wellness',
+		description:
+			'We prioritize the health and well-being of our employees by providing comprehensive medical, dental, and vision insurance plans. We also offer wellness programs, gym memberships, and resources to support a healthy lifestyle.'
+	},
+	{
+		icon: briefcaseIcon,
+		alt: 'briefcase icon',
+		title: 'Retirement Planning',
+		description:
+			'YourBank is committed to helping employees plan for their future. We offer a retirement savings plan with a generous employer match to help them build a secure financial foundation for the long term.'
+	},
+	{
+		icon: documentsIcon,
+		alt: 'documents icon',
+		title: 'Work-Life Balance',
+		description:
+			'We understand the importance of maintaining a healthy work-life balance. YourBank offers flexible work arrangements, paid time off, parental leave, and other programs that support employees in managing their personal and professional commitments.'
+	}
+];
