2c9f5eaccc78d867bed04332515475159815f48b
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-26 11:59:26 +0100

    feat: About component

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+import Introduction from '../components/about.introduction/Introduction';
+function About() {
+	return (
+		<div className="mt-[1.88rem]">
+			<Introduction />
+		</div>
+	);
+}
+
+export default About;
