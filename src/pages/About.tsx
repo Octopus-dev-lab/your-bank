1c18582ec7f68d5313bca449258fd7a3c913f202
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-26 14:43:06 +0100

    feat: added PressReleases in About component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
 import Introduction from '../components/about.introduction/Introduction';
 import MissionVision from '../components/about.missionVision/MissionVision';
+import PressReleases from '../components/about.pressReleases/PressReleases';
 function About() {
 	return (
 		<div className="mt-[1.88rem]">
 			<Introduction />
 			<MissionVision />
+			<PressReleases />
 		</div>
 	);
 }
