4fdb70e1007a48f5c938edc5805d0f82aeba1a01
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-26 14:08:03 +0100

    feat: added MissionVision to About component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,10 @@
 import Introduction from '../components/about.introduction/Introduction';
+import MissionVision from '../components/about.missionVision/MissionVision';
 function About() {
 	return (
 		<div className="mt-[1.88rem]">
 			<Introduction />
+			<MissionVision />
 		</div>
 	);
 }
