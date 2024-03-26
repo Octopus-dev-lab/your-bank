0c11216692744fdde97bb08d2a60245f34c39a56
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-26 14:54:33 +0100

    feat: Security component

---
+++ src/pages/Security.tsx
@@ -0,0 +1,11 @@
+import Introduction from '../components/security.introduction/introduction';
+
+function Security() {
+	return (
+		<div className="mt-12 mb-20">
+			<Introduction />
+		</div>
+	);
+}
+
+export default Security;
