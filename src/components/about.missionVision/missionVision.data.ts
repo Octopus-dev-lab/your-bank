03abb43cc1ef80f10250ef5acc8b4a14e10c4435
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-26 14:07:29 +0100

    feat: mission&vision data

---
+++ src/components/about.missionVision/missionVision.data.ts
@@ -0,0 +1,19 @@
+import missionImage from '../../assets/about.missionVision/mission.webp';
+import visionImage from '../../assets/about.missionVision/vision.webp';
+
+export default [
+	{
+		img: missionImage,
+		alt: 'Mission',
+		title: 'Mission',
+		content:
+			'At YourBank, our mission is to empower our customers to achieve financial success. We are dedicated to delivering innovative banking solutions that cater to their unique needs. Through personalized services, expert guidance, and cutting-edge technology, we aim to build strong, lasting relationships with our customers. Our mission is to be their trusted partner, helping them navigate their financial journey and realize their dreams.'
+	},
+	{
+		img: visionImage,
+		alt: 'Vision',
+		title: 'Vision',
+		content:
+			'Our vision at YourBank is to redefine banking by creating a seamless and personalized experience for our customers. We envision a future where banking is accessible, transparent, and tailored to individual preferences. Through continuous innovation and collaboration, we strive to be at the forefront of the industry, setting new standards for customer-centric banking. Our vision is to be the preferred financial institution, known for our unwavering commitment to excellence, trust, and customer satisfaction.'
+	}
+];
