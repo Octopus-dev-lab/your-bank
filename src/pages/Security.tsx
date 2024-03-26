2c0d4ad98b0ddaf310c39c2c4f807ff179b9c98a
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-26 15:35:32 +0100

    feat: added Protection to Security component

--- src/pages/Security.tsx
+++ src/pages/Security.tsx
@@ -1,9 +1,11 @@
 import Introduction from '../components/security.introduction/introduction';
+import Protection from '../components/security.protection/Protection';
 
 function Security() {
 	return (
 		<div className="mt-12 mb-20">
 			<Introduction />
+			<Protection />
 		</div>
 	);
 }
