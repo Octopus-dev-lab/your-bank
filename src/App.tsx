648470532a480344bac0fe4d2d0841f5c8f120ae
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-26 16:41:50 +0100

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import About from './pages/About';
 import Authentication from './pages/Authentication';
-// import Security from './pages/Security';
-// import About from './pages/About';
-// import Careers from './pages/Careers';
-// import Home from './pages/Home';
+import Careers from './pages/Careers';
+import Home from './pages/Home';
+import Security from './pages/Security';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />
+	},
+	{
+		path: '/careers',
+		element: <Careers />
+	},
+	{
+		path: '/about',
+		element: <About />
+	},
+	{
+		path: '/security',
+		element: <Security />
+	},
+	{
+		path: '/login',
+		element: <Authentication />
+	},
+	{
+		path: '/signup',
+		element: <Authentication />
+	}
+]);
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			{/* <Home /> */}
-			{/* <Careers /> */}
-			{/* <About /> */}
-			{/* <Security /> */}
-			<Authentication />
+			<RouterProvider router={router} />
 			<Footer />
 		</>
 	);
