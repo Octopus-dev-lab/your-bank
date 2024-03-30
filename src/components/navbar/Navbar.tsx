6e5ad33274a4c1a8b0973f3ef75c94ee42dee0c4
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-03-30 11:40:09 +0100

    feat: navbar responsive

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import hamburgerIcon from '../../assets/navbar/hamburger.svg';
 import logo from '../../assets/navbar/logo.svg';
 import logoText from '../../assets/navbar/your-bank.svg';
 import MobileMenu from './MobileMenu';
+import { authenticationPages, pages } from './navbar.data';
 
 function Navbar() {
 	const [isOpen, setIsOpen] = useState(false);
@@ -23,7 +24,7 @@ function Navbar() {
 			<div
 				className={`absolute z-50 w-[92%] mx-[calc(8%/2)] py-[0.875rem] pl-6 pr-[0.875rem] flex flex-col items-center bg-grey-900 ${isOpen ? 'rounded-[2rem]' : 'rounded-full'} border border-grey-800`}
 			>
-				<div className="w-full flex justify-between">
+				<div className="w-full flex justify-between items-center">
 					<Link
 						to={'/'}
 						onClick={() => setIsOpen(false)}
@@ -38,9 +39,33 @@ function Navbar() {
 							alt="YourBank name"
 						/>
 					</Link>
+					<nav className="hidden md:block">
+						<ul className="flex gap-3">
+							{pages.map((page, index) => (
+								<li
+									key={index}
+									className="text-white-100 text-[0.875rem] leading-[1.3125rem] font-normal lg:text-[1.125rem] lg:leading-[1.6875rem]"
+								>
+									<Link to={page.link}>{page.name}</Link>
+								</li>
+							))}
+						</ul>
+					</nav>
+					<nav className="hidden md:block">
+						<ul className="flex gap-3 text-white-100 text-[0.875rem] leading-[1.3125rem] font-normal lg:text-[1.125rem] lg:leading-[1.6875rem]">
+							{authenticationPages.map((page, index) => (
+								<li
+									className={`px-6 py-3 ${page.name === 'Login' ? 'bg-green-800 text-grey-900' : ''} rounded-full lg:px-[1.88rem] lg:py-[0.88rem]`}
+									key={index}
+								>
+									<Link to={page.link}>{page.name}</Link>
+								</li>
+							))}
+						</ul>
+					</nav>
 					<button
 						onClick={toggleMenu}
-						className={`px-3 py-1 rounded-full ${isOpen ? '' : 'bg-green-800'} `}
+						className={`px-3 py-1 rounded-full ${isOpen ? '' : 'bg-green-800'} md:hidden`}
 					>
 						<img
 							src={isOpen ? closeIcon : hamburgerIcon}
