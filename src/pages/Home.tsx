93bc675f6cb42d938b8459d5ff4151e607d890ce
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-04-04 16:27:42 +0200

    fix: in lg second section visible on page load

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,13 +9,13 @@ import Testimonials from '../components/testimonials/Testimonials';
 
 function Home() {
 	return (
-		<div className="mt-12 mb-20 sm:max-w-[640px] sm:mx-auto lg:max-w-[1400px] 2xl:max-w-[2000px]">
+		<div className="mt-12 mb-20 sm:max-w-[640px] sm:mx-auto lg:mt-0 lg:max-w-[1400px] 2xl:max-w-[2000px]">
 			<img
 				className="absolute top-0 left-0 -z-10 w-[23.87481rem] lg:w-[34.17163rem] 2xl:w-[47.43738rem] min-[2000px]:hidden"
 				src={abstract}
 				alt="abstract design"
 			/>
-			<div className="lg:w-full lg:flex lg:justify-between lg:px-20 2xl:px-40">
+			<div className="lg:w-full lg:min-h-[85vh] lg:flex lg:justify-between lg:items-center lg:px-20 2xl:px-40">
 				<Introduction />
 				<Preview />
 			</div>
