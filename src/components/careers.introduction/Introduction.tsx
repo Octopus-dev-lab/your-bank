7837f70727af7150e115d86788b3131eb1ca0d05
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-04-04 10:22:50 +0200

    fix: wrong arbitrary breakpoint

--- src/components/careers.introduction/Introduction.tsx
+++ src/components/careers.introduction/Introduction.tsx
@@ -5,7 +5,7 @@ function Introduction() {
 	return (
 		<div className="w-full px-4 lg:px-20 2xl:px-40">
 			<DualPaneCard image={image}>
-				<h1 className="text-white-100 text-[1.75rem] leading-[2.275rem] font-medium text-center mb-[0.88rem] lg:text-[2.375rem] lg:leading-[3.5625rem] xl:mb-5 xl:text-[3rem] xl:leading-[3.9rem] min-[1150px]:text-start 2xl:text-[3.625rem] 2xl:leading-[4.7125rem]">
+				<h1 className="text-white-100 text-[1.75rem] leading-[2.275rem] font-medium text-center mb-[0.88rem] lg:text-[2.375rem] lg:leading-[3.5625rem] xl:mb-5 xl:text-[3rem] xl:leading-[3.9rem] min-[1180px]:text-start 2xl:text-[3.625rem] 2xl:leading-[4.7125rem]">
 					Welcome to <span className="text-green-800">YourBank</span>{' '}
 					Careers!
 				</h1>
