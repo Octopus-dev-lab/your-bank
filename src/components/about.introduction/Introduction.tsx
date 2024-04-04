285c25316946ae526a907ae3cab45a8bf50438bf
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-04-04 11:32:40 +0200

    fix: missing max-w for lg

--- src/components/about.introduction/Introduction.tsx
+++ src/components/about.introduction/Introduction.tsx
@@ -3,15 +3,15 @@ import DualPaneCard from '../shared/DualPaneCard';
 
 function Introduction() {
 	return (
-		<div className="w-full p-4">
+		<div className="w-full p-4 lg:px-20 2xl:px-40">
 			<DualPaneCard image={image}>
-				<h2 className="mb-[0.12rem] text-white-100 text-[0.875rem] leading-[1.3125rem] font-normal text-center">
+				<h2 className="mb-[0.12rem] text-white-100 text-[0.875rem] leading-[1.3125rem] font-normal text-center lg:text-[1.125rem] lg:leading-[1.6875rem] min-[1180px]:text-start 2xl:text-[1.25rem] 2xl:leading-[1.875rem]">
 					Welcome to YourBank
 				</h2>
-				<h1 className="text-white-100 text-[1.75rem] leading-[2.275rem] font-medium text-center mb-[0.88rem] after:content-['_Excellence!'] after:text-green-800">
+				<h1 className="text-white-100 text-[1.75rem] leading-[2.275rem] font-medium text-center mb-[0.88rem] after:content-['_Excellence!'] after:text-green-800 lg:text-[2.375rem] lg:leading-[3.5625rem] xl:mb-5 xl:text-[3rem] xl:leading-[3.9rem] min-[1180px]:text-start 2xl:text-[3.625rem] 2xl:leading-[4.7125rem]">
 					Where Banking Meets
 				</h1>
-				<p className="text-grey-200 text-[0.875rem] leading-[1.3125rem] font-light text-center">
+				<p className="text-grey-200 text-[0.875rem] leading-[1.3125rem] font-light text-center lg:text-[1rem] lg:leading-6 min-[1180px]:text-start 2xl:text-[1.125rem] 2xl:leading-[1.6875rem]">
 					At YourBank, we believe that banking should be more than
 					just transactions. It should be an experience that empowers
 					individuals and businesses to thrive and reach their
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ import MissionVision from '../components/about.missionVision/MissionVision';
 import PressReleases from '../components/about.pressReleases/PressReleases';
 function About() {
 	return (
-		<div className="mt-[1.88rem] mb-20">
+		<div className="mt-[1.88rem] mb-20 sm:max-w-[640px] sm:mx-auto lg:max-w-[1400px] 2xl:max-w-[2000px]">
 			<Introduction />
 			<MissionVision />
 			<PressReleases />
