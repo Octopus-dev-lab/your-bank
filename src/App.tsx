a355e6d55640991780296892377487777caf1ea2
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-05-21 13:38:44 +0200

    feat: register useGSAP hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+
+gsap.registerPlugin(useGSAP);
 
 function App() {
 	return (
