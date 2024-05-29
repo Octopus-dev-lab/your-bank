a4b2b3adbfed2fcfb2f9aec8042f984658898891
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-05-29 17:02:11 +0200

    feat: Logo svg component

---
+++ src/components/shared/Logo.tsx
@@ -0,0 +1,27 @@
+import { ReactSVG } from 'react-svg';
+import logo from '../../assets/logo/logo.svg';
+
+function Logo({ svgSizeClasses }: { svgSizeClasses: string }) {
+	const classList = svgSizeClasses.split(' ');
+
+	return (
+		<ReactSVG
+			src={logo}
+			beforeInjection={(svg) => {
+				svg.classList.add(...classList);
+			}}
+			fallback={() => (
+				<div className="w-[6.33rem] h-[1.63rem] lg:w-[7.3rem] lg:h-[1.88rem] 2xl:w-[9.74rem] 2xl:h-[2.5rem]">
+					<img
+						className="text-white-100 text-[0.875rem] leading-[1.3125rem] font-normal 2xl:text-[1.125rem] 2xl:leading-[1.6875rem]"
+						src=""
+						alt="Your bank logo"
+					/>
+				</div>
+			)}
+			title="your bank logo"
+		/>
+	);
+}
+
+export default Logo;
