dfd5cb21703fbef56af6056683e7f6060ba3a61e
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-05-29 17:01:14 +0200

    buld: removed base used in production

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: '/your-bank/'
+	plugins: [react()]
+	// Uncomment the following line before building for production (subdomain deployment)
+	// base: '/your-bank/'
 });
