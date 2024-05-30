b03894eb4d39726cea33048613703ce54d1068db
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-05-30 11:12:22 +0200

    fix: alt contains useless info

--- src/components/products/products.data.ts
+++ src/components/products/products.data.ts
@@ -6,21 +6,21 @@ export default [
 	{
 		icon: briefcaseIcon,
 		title: 'Checking Accounts',
-		alt: 'Briefcase icon',
+		alt: 'Briefcase',
 		description:
 			'Enjoy easy and convenient access to your funds with our range of checking account options. Benefit from features such as online and mobile banking, debit cards, and free ATM access.'
 	},
 	{
 		icon: boxIcon,
 		title: 'Saving Accounts',
-		alt: 'Box icon with an arrow pointing down inside it',
+		alt: 'Box with an arrow pointing down inside it',
 		description:
 			"Build your savings with our competitive interest rates and flexible savings account options. Whether you're saving for a specific goal or want to grow your wealth over time, we have the right account for you."
 	},
 	{
 		icon: cashIcon,
 		title: 'Loans and Mortgages',
-		alt: 'Cash icon',
+		alt: 'Cash',
 		description:
 			'Realize your dreams with our flexible loan and mortgage options. From personal loans to home mortgages, our experienced loan officers are here to guide you through the application process and help you secure the funds you need.'
 	}
