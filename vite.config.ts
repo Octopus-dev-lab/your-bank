222e23b98a68ed983f63f0dfa788d5e4297ab87e
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-05-30 19:04:30 +0200

    build: added svg to config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [svgr(), react()]
 	// Uncomment the following line before building for production (subdomain deployment)
 	// base: '/your-bank/'
 });
