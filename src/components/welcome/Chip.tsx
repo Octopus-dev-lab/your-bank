0c005564f6687f86ee14ec2f1f7db01d937cfc54
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-05-31 13:58:25 +0200

    feat: from ReactSVG to svg component welcome

--- src/components/welcome/Chip.tsx
+++ src/components/welcome/Chip.tsx
@@ -1,7 +1,6 @@
 import { useGSAP } from '@gsap/react';
 import { useRef } from 'react';
-import { ReactSVG } from 'react-svg';
-import checkIcon from '../../assets/svg/check.svg';
+import CheckIcon from '../../assets/svg/check.svg?react';
 
 function Chip({ timeline }: { timeline: gsap.core.Timeline | undefined }) {
 	const chipRef = useRef<HTMLDivElement>(null);
@@ -25,20 +24,7 @@ function Chip({ timeline }: { timeline: gsap.core.Timeline | undefined }) {
 			ref={chipRef}
 			className="w-fit pl-2 pr-4 py-2 mb-3 gap-1 flex justify-center items-center bg-grey-800 rounded-full 2xl:py-[0.62rem] 2xl:px-5 2xl:gap-[0.38rem]"
 		>
-			<ReactSVG
-				className="w-5 h-5 2xl:w-6 2xl:h-6"
-				src={checkIcon}
-				beforeInjection={(svg) => {
-					svg.classList.add('w-5', 'h-5', '2xl:w-6', '2xl:h-6');
-				}}
-				fallback={() => (
-					<img
-						className="text-white-100 text-xs leading-6 font-light lg:text-[0.875rem] lg:leading-6 2xl:text-[1.125rem] 2xl:leading-6 overflow-visible"
-						src=""
-						alt="Check icon"
-					/>
-				)}
-			/>
+			<CheckIcon className="w-5 h-5 2xl:w-6 2xl:h-6" />
 			<p className="text-white-100 text-xs leading-6 font-light lg:text-[0.875rem] lg:leading-6 2xl:text-[1.125rem] 2xl:leading-6">
 				No LLC Required, No Credit Check.
 			</p>
