22f3a77435640bd035799635f736018fa9293a9f
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-05-31 13:57:13 +0200

    feat: from ReactSVG to svg component features

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -1,4 +1,4 @@
-import ArrowLink from './ArrowLink';
+import ArrowLink from '../../assets/svg/arrow-link.svg?react';
 
 function Features({
 	content
@@ -16,7 +16,7 @@ function Features({
 						<h3 className="text-white-100 text-[1.125rem] leading-[1.6875rem] font-normal 2xl:text-[1.375rem] 2xl:leading-[2.0625rem]">
 							{feature.title}
 						</h3>
-						<ArrowLink />
+						<ArrowLink className="w-6 h-6 2xl:w-[2.125rem] 2xl:h-[2.125rem]" />
 					</div>
 					<p className="text-grey-200 text-[0.875rem] leading-[1.3125] font-light -tracking-[0.00525rem] lg:text-[1rem] lg:leading-6 2xl:text-[1.125rem] 2xl:leading-[1.6875rem]">
 						{feature.description}
