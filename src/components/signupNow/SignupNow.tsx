1672169b60f52adbab37f08e9a4fef567d75edd9
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-05-31 13:57:47 +0200

    feat: from ReactSVG to svg component signupNow

--- src/components/signupNow/SignupNow.tsx
+++ src/components/signupNow/SignupNow.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import RadialAbstract from '../shared/RadialAbstract';
+import RadialAbstract from '../../assets/svg/radial-abstract.svg?react';
 
 function SignupNow() {
 	return (
