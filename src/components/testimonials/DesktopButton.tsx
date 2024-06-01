ab9d0a8171379f84a88ef4b0e2dce7576348db68
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-06-01 19:44:11 +0200

    fix: missing heights and responsive sizes

--- src/components/testimonials/DesktopButton.tsx
+++ src/components/testimonials/DesktopButton.tsx
@@ -12,7 +12,9 @@ function DesktopButton({
 			onClick={onClick}
 			className={`hidden min-w-max xl:block p-3 rounded-full border border-grey-800 bg-grey-900 2xl:p-[0.88rem] z-20 ${isNext ? 'ml-[3.12rem]' : 'mr-[3.12rem]'}`}
 		>
-			<Arrow className={isNext ? 'transform rotate-180' : ''} />
+			<Arrow
+				className={`${isNext && 'transform rotate-180'} w-7 h-7 2xl:w-[2.125rem] 2xl:h-[2.125rem]`}
+			/>
 		</button>
 	);
 }
--- src/components/testimonials/MobileButtons.tsx
+++ src/components/testimonials/MobileButtons.tsx
@@ -13,13 +13,13 @@ function MobileButtons({
 				onClick={onClickLeft}
 				className="p-3 mr-3 rounded-full border border-grey-800 bg-grey-900 lg:mr-14"
 			>
-				<Arrow />
+				<Arrow className="w-7 h-7" />
 			</button>
 			<button
 				onClick={onClickRight}
 				className="p-3 ml-[10px] rounded-full border border-grey-800 bg-grey-900"
 			>
-				<Arrow className="transform rotate-180" />
+				<Arrow className="w-7 h-7 transform rotate-180" />
 			</button>
 		</div>
 	);
