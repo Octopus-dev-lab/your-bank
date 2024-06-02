4ea367f9e12614e003ea1444b2a86498b68f2c00
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-06-02 12:15:18 +0200

    fix: replaced ReactSVG with SVG component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
-import { ReactSVG } from 'react-svg';
-import abstract from '../assets/svg/dot-abstract.svg';
+import AbstractSVG from '../assets/svg/dot-abstract.svg?react';
 import FaqsSection from '../components/faqs/FaqsSection';
 import FeaturesSection from '../components/features/FeaturesSection';
 import PreviewSection from '../components/preview/PreviewSection';
@@ -11,27 +10,7 @@ import WelcomeSection from '../components/welcome/WelcomeSection';
 function Home() {
 	return (
 		<div className="mt-12 mb-20 sm:max-w-[640px] sm:mx-auto lg:mt-0 lg:max-w-[1400px] 2xl:max-w-[2000px]">
-			<ReactSVG
-				className="absolute top-0 left-0 -z-10 w-[23.87481rem] h-[19.21356rem] lg:w-[34.17163rem] lg:h-[27.5rem] 2xl:w-[47.43738rem] 2xl:h-[38.17581rem] min-[2000px]:hidden"
-				src={abstract}
-				beforeInjection={(svg) => {
-					svg.classList.add(
-						'w-[23.87481rem]',
-						'h-[19.21356rem]',
-						'lg:w-[34.17163rem]',
-						'lg:h-[27.5rem]',
-						'2xl:w-[47.43738rem]',
-						'2xl:h-[38.17581rem]'
-					);
-				}}
-				fallback={() => (
-					<img
-						className="text-white-100 text-xs leading-6 font-light lg:text-[0.875rem] lg:leading-6 2xl:text-[1.125rem] 2xl:leading-6 overflow-visible"
-						src=""
-						alt="abstract design made of dots"
-					/>
-				)}
-			/>
+			<AbstractSVG className="absolute top-0 left-0 -z-10 w-[23.87481rem] h-[19.21356rem] lg:w-[34.17163rem] lg:h-[27.5rem] 2xl:w-[47.43738rem] 2xl:h-[38.17581rem] min-[2000px]:hidden" />
 			<div className="lg:w-full lg:min-h-[85vh] lg:flex lg:justify-between lg:items-center lg:px-20 2xl:px-40">
 				<WelcomeSection />
 				<PreviewSection />
