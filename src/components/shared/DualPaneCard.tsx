f0f5948245f3e68c4b94dd325aa0444276df5b8c
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-06-02 15:57:33 +0200

    fix: DualPaneCard uses img instead of picture

--- src/components/shared/DualPaneCard.tsx
+++ src/components/shared/DualPaneCard.tsx
@@ -1,25 +1,35 @@
-import dottedAbstractDesign from '../../assets/careers.introduction/dotted-abstract.svg';
+import RadialAbstract from '../../assets/svg/radial-abstract.svg?react';
 
 function DualPaneCard({
-	image,
+	imageSm,
+	imageMd,
+	imageLg,
 	children
 }: {
-	image: string;
+	imageSm: string;
+	imageMd: string;
+	imageLg: string;
 	children: React.ReactNode;
 }) {
 	return (
 		<div className="relative w-full p-[0.88rem] rounded-2xl bg-grey-900 md:p-5 lg:max-w-[640px] lg:mx-auto min-[1180px]:p-10 min-[1180px]:max-w-none 2xl:p-[3.12rem]">
-			<img
-				className="absolute top-0 right-0 z-10 lg:w-[20.875rem] 2xl:w-[31.5625rem]"
-				src={dottedAbstractDesign}
-				alt="Dotted abstract design"
-			/>
+			<RadialAbstract className="absolute top-0 right-0 w-[12.4375rem] h-[11.8125rem] z-10 lg:w-[20.875rem] lg:h-[19.8125rem] 2xl:w-[31.5625rem] 2xl:h-[30rem] rotate-90" />
 			<div className="relative w-full z-20 min-[1180px]:flex min-[1180px]:flex-row-reverse">
-				<img
-					className="relative top-0 left-0 w-full rounded-t-2xl min-[1180px]:min-w-[66.666667%] min-[1180px]:w-2/3 min-[1180px]:self-start min-[1180px]:flex-auto"
-					src={image}
-					alt="Careers introduction image"
-				/>
+				<picture className="relative top-0 left-0 w-full min-[1180px]:min-w-[66.666667%] min-[1180px]:w-2/3 min-[1180px]:self-start min-[1180px]:flex-auto">
+					<source
+						media="(min-width: 1180px)"
+						srcSet={imageLg}
+					/>
+					<source
+						media="(min-width: 430px)"
+						srcSet={imageMd}
+					/>
+					<img
+						className="w-full rounded-2xl"
+						src={imageSm}
+						alt="Job interview"
+					/>
+				</picture>
 				<div className="relative w-full -mt-6 p-6 z-10 rounded-3xl bg-grey-1000 md:-mt-16 md:p-8 min-[1180px]:p-[3.75rem] min-[1180px]:min-w-[50%] min-[1180px]:w-1/2 min-[1180px]:h-min min-[1180px]:mt-0 min-[1180px]:-right-[16.666667%] min-[1180px]:rounded-tr-[0] min-[1180px]:rounded-l-[1.25rem] min-[1180px]:rounded-br-[3.75rem] 2xl:p-20 2xl:rounded-br-[5rem]">
 					{children}
 				</div>
