ecc5a57943ef914e987e46ec9bd0549559cbc4f5
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-06-04 19:37:31 +0200

    fix: removed useless class from chip

--- src/components/welcome/Chip.tsx
+++ src/components/welcome/Chip.tsx
@@ -22,7 +22,7 @@ function Chip({ timeline }: { timeline: gsap.core.Timeline | undefined }) {
 	return (
 		<div
 			ref={chipRef}
-			className="w-fit pl-2 pr-4 py-2 mb-3 gap-1 flex justify-center items-center bg-grey-800 rounded-full 2xl:py-[0.62rem] 2xl:px-5 2xl:gap-[0.38rem]"
+			className="pl-2 pr-4 py-2 mb-3 gap-1 flex justify-center items-center bg-grey-800 rounded-full 2xl:py-[0.62rem] 2xl:px-5 2xl:gap-[0.38rem]"
 		>
 			<CheckIcon className="w-5 h-5 2xl:w-6 2xl:h-6" />
 			<p className="text-white-100 text-xs leading-6 font-light lg:text-[0.875rem] lg:leading-6 2xl:text-[1.125rem] 2xl:leading-6">
