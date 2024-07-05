c70e4e7a555a4a2a66547bf7f268aa3a0d810ab0
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-07-05 11:45:21 +0200

    feat: added back to top button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
-import { Outlet } from 'react-router-dom';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useLayoutEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import BackToTop from './components/shared/BackToTop';
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollToPlugin);
 
 function App() {
+	const location = useLocation();
+
+	useLayoutEffect(() => {
+		console.log('refresh');
+		ScrollTrigger.refresh();
+	}, [location]);
+
 	return (
 		<>
 			<Navbar />
 			<Outlet />
 			<Footer />
+			<BackToTop />
 		</>
 	);
 }
