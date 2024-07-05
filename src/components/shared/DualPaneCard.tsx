28d11ae585e46a75c73390065d195748e2b14616
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-07-05 11:37:29 +0200

    fix: no height specified in DualPaneCard

--- src/components/shared/DualPaneCard.tsx
+++ src/components/shared/DualPaneCard.tsx
@@ -12,10 +12,10 @@ function DualPaneCard({
 	children: React.ReactNode;
 }) {
 	return (
-		<div className="relative w-full p-[0.88rem] rounded-2xl bg-grey-900 md:p-5 lg:max-w-[640px] lg:mx-auto min-[1180px]:p-10 min-[1180px]:max-w-none 2xl:p-[3.12rem]">
+		<div className="relative w-full h-min p-[0.88rem] rounded-2xl bg-grey-900 md:p-5 lg:max-w-[640px] lg:mx-auto min-[1180px]:p-10 min-[1180px]:max-w-none 2xl:p-[3.12rem]">
 			<RadialAbstract className="absolute top-0 right-0 w-[12.4375rem] h-[11.8125rem] z-10 lg:w-[20.875rem] lg:h-[19.8125rem] 2xl:w-[31.5625rem] 2xl:h-[30rem] rotate-90" />
 			<div className="relative w-full z-20 min-[1180px]:flex min-[1180px]:flex-row-reverse">
-				<picture className="relative top-0 left-0 w-full min-[1180px]:min-w-[66.666667%] min-[1180px]:w-2/3 min-[1180px]:self-start min-[1180px]:flex-auto">
+				<picture className="relative top-0 left-0 w-full min-[1180px]:min-w-[66.666667%] min-[1180px]:w-2/3 min-[1180px]:self-start min-[1180px]:flex-auto ">
 					<source
 						media="(min-width: 1180px)"
 						srcSet={imageLg}
@@ -25,7 +25,7 @@ function DualPaneCard({
 						srcSet={imageMd}
 					/>
 					<img
-						className="w-full rounded-2xl"
+						className="w-full aspect-[1.3] rounded-2xl object-cover"
 						src={imageSm}
 						alt="Job interview"
 					/>
