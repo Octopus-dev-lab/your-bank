11e47d152e3e187b515b32fa4e2e184139ed858e
JurgenCS <129676759+JurgenCS@users.noreply.github.com>
2024-07-05 10:56:17 +0200

    refactor: white space between imports and component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 import AboutSection from '../components/about/AboutSection';
 import MissionSection from '../components/mission/MissionSection';
 import PressReleasesSection from '../components/pressReleases/PressReleasesSection';
+
 function About() {
 	return (
 		<div className="mt-[1.88rem] mb-20 sm:max-w-[640px] sm:mx-auto lg:max-w-[1400px] 2xl:max-w-[2000px]">
